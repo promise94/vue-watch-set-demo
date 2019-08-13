78d84adc7b4cbd8f162cdb23c63296de5eb19cca
lixiaowei <promise9495@163.com>
2019-08-13 09:29:41 +0800

    set watch demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <p>{{ JSON.stringify(set) }}</p>
+    <button @click="addSet">ADD</button>
+    <ul>
+      <li v-for="(row, index) of n" :key="index" v-text="row"></li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { Component, Vue, Watch } from "vue-property-decorator";
 
-@Component({
-  components: {
-    HelloWorld
+@Component({})
+export default class Home extends Vue {
+  private set: any = new Set();
+  private n: number = 0;
+
+  @Watch("set")
+  setChange(val: any, oldVal: any) {
+    console.log(val, "--", oldVal);
   }
-})
-export default class Home extends Vue {}
+
+  addSet() {
+    ++this.n;
+    this.set.add(this.n);
+  }
+}
 </script>
+
+<style lang="less" scoped>
+button {
+  user-select: none;
+}
+li {
+  list-style: none;
+}
+</style>
